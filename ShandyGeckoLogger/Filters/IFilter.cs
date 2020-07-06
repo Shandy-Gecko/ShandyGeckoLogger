5af9b3d45a7c5178319678b38c4857bb62320ea9
Илья Гутников <ilyagutnikov93@gmail.com>
2020-07-06 20:01:11 +0300

    Re-init

--- ShandyGeckoLogger/Filters/IFilter.cs
+++
@@ -1,8 +0,0 @@
-namespace LowPolyShooter.LogSystem.Filters
-{
-	public interface IFilter
-	{
-		bool IsPassed(MessageType messageType, string tag, string msg);
-		bool IsPassed(MessageType messageType, object obj, string msg);
-	}
-}
--- ShandyGeckoLogger/Formatter/IFormatterValueProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace LowPolyShooter.LogSystem
-{
-	public interface IFormatterValueProvider
-	{
-		string Name { get; }
-		string GetValue();
-	}
-}
